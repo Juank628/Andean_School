e8346da15df4837a06fd1017960aefdab391e9fd
Juank628 <jsanchez_zunino@yahoo.com>
2019-08-30 22:00:40 -0500

    news no carousel

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,9 +2,9 @@ let navMenu = document.querySelector(".nav-menu");
 let navMenuBtn = document.querySelector(".nav-menu-btn");
 
 navMenuBtn.addEventListener("click", () => {
-  if (navMenu.style.display === "flex") {
-    navMenu.style.display = "none";
+  if (navMenu.className === "nav-menu nav-menu-show") {
+    navMenu.className = "nav-menu nav-menu-hidden"
   } else {
-    navMenu.style.display = "flex";
+    navMenu.className = "nav-menu nav-menu-show"
   }
 });
